2141d99082b5010f934f60b3e45e5965832a6ac6
UNIVERSAL STANDARD (OFFICIAL) <171918994+UniversalStandards@users.noreply.github.com>
2025-09-13 02:43:07 -0700

    Update main.py
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def home():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=DEBUG)
+    app.run(host='127.0.0.1', port=port, debug=DEBUG)
 
 
 
